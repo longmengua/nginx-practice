bae0a0e3758cb08cd58b2e89c9fd912a416a963c
Waltor <longmengua@gmail.com>
2024-02-29 14:29:49 +0800

    cicd: update

--- server.sh
+++ server.sh
@@ -1,7 +1,31 @@
+function display_error {
+    echo "Error: $1"
+    exit 1
+}
+
+function common_steps {
+    # Step 1
+    # echo "===> Start pull code from $1 branch"
+    git fetch
+    # git checkout $1
+    git pull
+
+    # Step 2
+    echo "===> Check version in composer.json"
+    json_file="./composer.json"
+    if [ ! -f "$json_file" ]; then
+        display_error "File not found: $json_file"
+    fi
+
+    version=$(grep '"version":' "$json_file" | sed -E 's/.*"version": *"([^"]+).*/\1/')
+    echo -e "\nVersion: $version\n"
+}
+
 function launchNestServer {
   cd nest-server
   docker build -t nest-server .
   docker rm -f nest-server && docker run -d -p 3000:3000 --name nest-server --network nginx-net nest-server
 }
 
+common_steps
 launchNestServer
--- web.sh
+++ web.sh
@@ -1,3 +1,25 @@
+function display_error {
+    echo "Error: $1"
+    exit 1
+}
+
+function common_steps {
+    # Step 1
+    # echo "===> Start pull code from $1 branch"
+    git fetch
+    # git checkout $1
+    git pull
+
+    # Step 2
+    echo "===> Check version in composer.json"
+    json_file="./composer.json"
+    if [ ! -f "$json_file" ]; then
+        display_error "File not found: $json_file"
+    fi
+
+    version=$(grep '"version":' "$json_file" | sed -E 's/.*"version": *"([^"]+).*/\1/')
+    echo -e "\nVersion: $version\n"
+}
 
 function launchVueApp {
   cd vue-app
@@ -5,4 +27,5 @@ function launchVueApp {
   docker rm -f vue-app && docker run -d -p 80:80 --name vue-app --network nginx-net vue-app 
 }
 
+common_steps
 launchVueApp
